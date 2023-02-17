78e6c25e043be806cd249adfe71f226c88cec807
bispray <53886563+bispray@users.noreply.github.com>
2023-02-17 09:42:12 +0800

    Add files via upload

---
+++ hexo-butterfly-footer-beautify/runtime.js
@@ -0,0 +1,38 @@
+function runtime_editor(){
+  let create_time = Math.round(new Date('2023-02-15 00:00:00').getTime() / 1000); //在此行修改建站时间
+  let timestamp = Math.round((new Date().getTime()) / 1000);
+  let second = timestamp - create_time;
+  let time = new Array(0, 0, 0, 0, 0);
+
+  var nol = function(h){
+    return h>9?h:'0'+h;
+  }
+  if (second >= 365 * 24 * 3600) {
+    time[0] = parseInt(second / (365 * 24 * 3600));
+    second %= 365 * 24 * 3600;
+  }
+  if (second >= 24 * 3600) {
+    time[1] = parseInt(second / (24 * 3600));
+    second %= 24 * 3600;
+  }
+  if (second >= 3600) {
+    time[2] = nol(parseInt(second / 3600));
+    second %= 3600;
+  }
+  if (second >= 60) {
+    time[3] = nol(parseInt(second / 60));
+    second %= 60;
+  }
+  if (second > 0) {
+    time[4] = nol(second);
+  }
+  if ((Number(time[2])<20) && (Number(time[2])>7)){
+    currentTimeHtml ="<img class='boardsign' src='https://img.shields.io/badge/两朵浪花-早安-6adea8?style=social&logo=cakephp' title='愿漂泊的人都有酒喝，愿拼搏的人都有收获'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + '</div>';
+  }
+  else{
+    currentTimeHtml ="<img class='boardsign' src='https://img.shields.io/badge/两朵浪花-晚安-6adea8?style=social&logo=coffeescript' title='愿漂泊的人都有酒喝，愿拼搏的人都有收获'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + '</div>';
+  }
+  document.getElementById("workboard").innerHTML = currentTimeHtml;
+}
+runtime_editor();
+setInterval(runtime_editor(), 300000);
